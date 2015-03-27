0ca0a645de0d1970669c60989e3e2f6e58393ee7
AcesoUnderGlass <elizabeth@acesounderglass.com>
2015-03-27 12:56:58 -0700

    Working EasyMock Tests
    Fills in mock tests with proper easymock created objects, rather than
    hand mocks.

--- app/src/androidTest/java/com/acesounderglass/hungertracker/ActivityTest.java
+++ app/src/androidTest/java/com/acesounderglass/hungertracker/ActivityTest.java
@@ -1,6 +1,5 @@
 package com.acesounderglass.hungertracker;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
 import android.view.View;
@@ -8,9 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import static org.easymock.EasyMock.*;
-import org.easymock.EasyMockRule;
-import org.easymock.TestSubject;
-import org.easymock.Mock;
 
 
 
@@ -21,23 +17,22 @@ public class ActivityTest extends ActivityInstrumentationTestCase2<MainActivity>
     private MainActivity activity;
     EditText inputText;
     TextView outputText;
-    HungerTrackerWriterMock mockWriter;
-    HungerTrackerWriter mockWriter2;
+    HungerTrackerWriter mockWriter;
 
     public ActivityTest() {
-
         super(MainActivity.class);
     }
 
     //TODO figure out how to do one time set up
     //TODO mock once, in set up, not in text (ew)
-    //TODO use mockito, not a hand mock
 
     public void setUp() throws Exception {
         super.setUp();
         setActivityInitialTouchMode(true);
         activity = getActivity();
         assertNotNull(activity);
+        getInstrumentation().getTargetContext().getCacheDir();
+        System.setProperty("dexmaker.dexcache", getInstrumentation().getTargetContext().getCacheDir().getPath());
 
 
         // Verify initial state before tests can alter it
@@ -55,7 +50,7 @@ public class ActivityTest extends ActivityInstrumentationTestCase2<MainActivity>
     }
 
     public void testCanInput() {
-        sendKeystrokesToInputBox(new int[] {
+        sendKeystrokesToInputBox(new int[]{
                 KeyEvent.KEYCODE_FORWARD_DEL,
                 KeyEvent.KEYCODE_6});
         assertEquals("6", inputText.getText().toString());
@@ -63,32 +58,27 @@ public class ActivityTest extends ActivityInstrumentationTestCase2<MainActivity>
 
     public void testStoreButton() {
         setMockWriter();
-        int startWriteToFileCount = mockWriter.getWriteToFileCount();
+        mockWriter.writeToFileWithDate("0");
+        replay(mockWriter);
         clickButton(R.id.store_button);
-        assertEquals(startWriteToFileCount+1, mockWriter.getWriteToFileCount());
+        verify(mockWriter);
     }
 
     public void testClearButton() {
         setMockWriter();
-        int startClearCount = mockWriter.getClearFileCount();
+        mockWriter.clearFile();
+        replay(mockWriter);
         clickButton(R.id.clear_button);
-        assertEquals(startClearCount+1, mockWriter.getClearFileCount());
-
+        verify(mockWriter);
     }
 
     public void testRetrieveButton() {
         setMockWriter();
-//        int startRetrieveCount = mockWriter.getRetrieveDataCount();
-//        clickButton(R.id.retrieve_button);
-//        assertEquals(startRetrieveCount+1, mockWriter.getRetrieveDataCount());
 
-        mockWriter2.retrieveData();
-        replay(mockWriter2);
+        expect(mockWriter.retrieveData()).andReturn("");
+        replay(mockWriter);
         clickButton(R.id.retrieve_button);
-        verify(mockWriter2);
-
-
-
+        verify(mockWriter);
     }
 
     private void testButtonExists(int id, String name) {
@@ -125,8 +115,8 @@ public class ActivityTest extends ActivityInstrumentationTestCase2<MainActivity>
     // Necessary because setUp() is run before each test.  Will go away when that is fixed
     private void setMockWriter() {
         if (mockWriter==null) {
-//            mockWriter = new HungerTrackerWriterMock("", activity.getBaseContext());
-            mockWriter2 = createMock(HungerTrackerWriter.class);
+
+            mockWriter = createMock(HungerTrackerWriter.class);
             activity.setWriter(mockWriter);
         }
     }
